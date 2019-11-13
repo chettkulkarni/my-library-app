5066931c66ffab334f392c8ac0c02c9ee66cfd3f
chettykulkarni <chettykulkarni@gmail.com>
2019-11-13 02:06:21 -0800

    a

---
+++ build/precache-manifest.2c163145fb4b340476468085b62764a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daee7d1f09d40bf56332613786921f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faaf36ac4625dc8380c6",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "ef95b34e873ca6e22983",
+    "url": "/static/css/main.5c86d63e.chunk.css"
+  },
+  {
+    "revision": "faaf36ac4625dc8380c6",
+    "url": "/static/js/2.5cb28569.chunk.js"
+  },
+  {
+    "revision": "ef95b34e873ca6e22983",
+    "url": "/static/js/main.fb86f09a.chunk.js"
+  },
+  {
+    "revision": "6b862321c6cbd95f4931",
+    "url": "/static/js/runtime-main.ab9d6a79.js"
+  }
+]);
