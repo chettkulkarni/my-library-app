106cd03ac9eb936060d7bbdb041832ea1da60434
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2019-11-24 22:01:54 -0800

    added image url and same changes to display year,etc

--- src/components/bookinfodesc.js
+++ src/components/bookinfodesc.js
@@ -1,110 +1,130 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import {Button,Jumbotron } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import firebase from 'firebase';
+import { Link } from "react-router-dom";
+import firebase from "firebase";
 // import {Link} from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import { withRouter} from 'react-router-dom';
-import axios from 'axios';
-import '../css/bookinfodesc.css'
+import Button from "@material-ui/core/Button";
+import { withRouter } from "react-router-dom";
+import axios from "axios";
+import "../css/bookinfodesc.css";
 // import Jumbotron from 'react-bootstrap/Jumbotron'
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
-
- class BookInfoDesc extends Component {
+class BookInfoDesc extends Component {
   //  constructor(props){
   //    super(props)
   //  }
-  state={isLoggedIn : false,book: [],button:''}
-  componentWillMount = () =>{
-    firebase.auth().onAuthStateChanged(user=>{
-        this.setState({isLoggedIn : !!user });
+  state = { isLoggedIn: false, book: [], button: "" };
+  componentWillMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isLoggedIn: !!user });
     });
-}
+  };
 
-  handleClick(e,id,title2) {
-    console.log('func called',id,title2,this.state.isLoggedIn)
-    if (this.state.isLoggedIn ){
-    var domain='http://lmp.nupursjsu.net/v1/'
-    var user=firebase.auth().currentUser.uid;
-    var url ="/request/"+user+"/"+id;
-    // var type="Type"
-    // console.log(user)
-    
-    //  user=1
-			var userInfo ='?Status=Approved,Issued&Book_Id='+id+'&User_Id='+user;
-			console.log(userInfo)
-			  url=domain+"requests"+userInfo;
-			  axios.get(url)
-			  .then(data => {
-					// console.log('data',data.data.length)
-			var alreadyTaken=0
-					if(data.data.length>0){
-					alreadyTaken=1;
+  handleClick(e, id, title2) {
+    console.log("func called", id, title2, this.state.isLoggedIn);
+    if (this.state.isLoggedIn) {
+      var domain = "http://lmp.nupursjsu.net/v1/";
+      var user = firebase.auth().currentUser.uid;
+      var url = "/request/" + user + "/" + id;
+      // var type="Type"
+      // console.log(user)
 
-			}
-      console.log('already taken',alreadyTaken)
-      if(alreadyTaken){
-        alert('only one book person sorry')
-      }
-          else{
-            var userInfo = {
-              'Type':'issue',
-              'bookID':id,
-              'title': title2,
-              'userId':user
-            };
-        axios.post(domain+'requests', userInfo)
-        .then(res => {
-          console.log(res);
-          console.log(res.data);
-          window.location.reload(false);
-        })
-        
-    }
-    
-
-    })
-  
-  
+      //  user=1
+      var userInfo =
+        "?Status=Approved,Issued&Book_Id=" + id + "&User_Id=" + user;
+      console.log(userInfo);
+      url = domain + "requests" + userInfo;
+      axios.get(url).then(data => {
+        // console.log('data',data.data.length)
+        var alreadyTaken = 0;
+        if (data.data.length > 0) {
+          alreadyTaken = 1;
+        }
+        console.log("already taken", alreadyTaken);
+        if (alreadyTaken) {
+          alert("only one book person sorry");
+        } else {
+          var userInfo = {
+            Type: "issue",
+            bookID: id,
+            title: title2,
+            userId: user
+          };
+          axios.post(domain + "requests", userInfo).then(res => {
+            console.log(res);
+            console.log(res.data);
+            window.location.reload(false);
+          });
+        }
+      });
     }
   }
 
-  render(){
-
-
-
-
+  render() {
     return (
-      <div  style={{ width: '100%' ,'margin-left':'5%','margin-right':'5%','margin-top':'5%'}}>
-
-
-<div class='w-100' >
-        {this.props.books.map((book,index) => (
-
-<div>
-            <Paper>
-
-            <h1>{book.title}</h1>
-             <b>Author: </b> <Link to={`/search/?title=&authors=${book.authors}&language=`} > {book.authors}</Link>
-                   <p><b>Rating:</b>{book.average_rating}</p>
-                   <p><b>BookID:</b>{book.bookID}</p>
-                   <p><b>Language Code:</b>{book.language_code}</p>
-                   <p><b>Rating Count:</b>{book.ratings_count}</p>
-                   <p><b>Text Reviews Count:</b>{book.text_reviews_count}</p>
-                   <p><b>Count:</b>{book.count}</p>
-                   <Button variant="outlined" style={{ color: '#0000ff' }}
-                   onClick={(e) => this.handleClick(e,`${book.bookID}`,`${book.title}`)}
-                   >{this.props.button}
-                   </Button>
-                   {/* <p>{this.state.toggleState}</p>   */}
-                   </Paper>
-           </div>
-        ))}
-        </div>
-
-</div>
-    )
+      <div
+        style={{
+          width: "100%",
+          "margin-left": "5%",
+          "margin-right": "5%",
+          "margin-top": "5%"
         }}
-  
-  export default withRouter(BookInfoDesc);
+      >
+        <div class="w-100">
+          {this.props.books.map((book, index) => (
+            <div>
+              <Paper>
+                <h1>{book.title}</h1>
+                <b>Author: </b>{" "}
+                <Link to={`/search/?title=&authors=${book.authors}&language=`}>
+                  {" "}
+                  {book.authors}
+                </Link>
+                <p>
+                  <b>Rating:</b>
+                  {book.average_rating}
+                </p>
+                <p>
+                  <b>BookID:</b>
+                  {book.bookID}
+                </p>
+                <p>
+                  <img src={book.image}></img>
+                </p>
+                <p>
+                  <b>Year Published</b>
+                  {book.year}
+                </p>
+                <p>
+                  <b>ISBN:</b>
+                  {book.isbn}
+                </p>
+                <p>
+                  <b>Publisher:</b>
+                  {book.publisher}
+                </p>
+                <p>
+                  <b>Count:</b>
+                  {book.count}
+                </p>
+                <Button
+                  variant="outlined"
+                  style={{ color: "#0000ff" }}
+                  onClick={e =>
+                    this.handleClick(e, `${book.bookID}`, `${book.title}`)
+                  }
+                >
+                  {this.props.button}
+                </Button>
+                {/* <p>{this.state.toggleState}</p>   */}
+              </Paper>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(BookInfoDesc);
--- src/components/books.js
+++ src/components/books.js
@@ -1,63 +1,86 @@
-import React from 'react'
-import CardDeck from 'react-bootstrap/CardDeck';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import  { Component } from 'react'
-import { withRouter} from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import CardDeck from "react-bootstrap/CardDeck";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import { Link } from "react-router-dom";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 // import axios from 'axios';
 
-
-
-
-   class Books  extends Component {
-
-
-   
-   render(){
+class Books extends Component {
+  render() {
     //  console.log(this.props.books2)
     return (
-       <div>
-         <center><h1>Book List</h1></center>
+      <div>
+        <center>
+          <h1>Book List</h1>
+        </center>
         <Container>
           <Row>
-         {this.props.books.map((book,index) => (
-         <Col md="auto">
-         <CardDeck>
-         <Card  style={{width: '20rem' }}>
-          <CardBody>
-            <CardTitle>{book.title}</CardTitle>
-            <CardText>
-              {/* <p><b>Author:</b>{book.authors}</p> */}
-              <b>Author: </b> <Link to={`/search/?title=&authors=${book.authors}&language=`} > {book.authors}</Link>
-                    <p><b>Rating:</b>{book.average_rating}</p>
-                    <p><b>BookID:</b>{book.bookID}</p>
-                    <p><b>Language Code:</b>{book.language_code}</p>
-                    <p><b>Rating Count:</b>{book.ratings_count}</p>
-                    <p><b>Text Reviews Count:</b>{book.text_reviews_count}</p>
-                    <p><b>Count:</b>{book.count}</p>
-              </CardText>
-            {/* <Link to={`/bookinfo/${book.bookID}`} > Click to View More</Link> */}
-            <Button variant="outlined" style={{ color: 'primary' }} href={`/bookinfo/${book.bookID}` } > Click to View More</Button>
-            </CardBody>
-          </Card>
-          </CardDeck>
-          </Col>
-         ))}
-
-         </Row>
+            {this.props.books.map((book, index) => (
+              <Col md="auto">
+                <CardDeck>
+                  <Card style={{ width: "20rem" }}>
+                    <CardBody>
+                      <CardTitle>{book.title}</CardTitle>
+                      <CardText>
+                        {/* <p><b>Author:</b>{book.authors}</p> */}
+                        <b>Author: </b>{" "}
+                        <Link
+                          to={`/search/?title=&authors=${book.authors}&language=`}
+                        >
+                          {" "}
+                          {book.authors}
+                        </Link>
+                        <p>
+                          <b>Rating:</b>
+                          {book.average_rating}
+                        </p>
+                        <p>
+                          <b>BookID:</b>
+                          {book.bookID}
+                        </p>
+                        <p>
+                          <img src={book.image}></img>
+                        </p>
+                        <p>
+                          <b>Year Published:</b>
+                          {book.year}
+                        </p>
+                        <p>
+                          <b>Authors:</b>
+                          {book.authors}
+                        </p>
+                        <p>
+                          <b>ISBN:</b>
+                          {book.isbn}
+                        </p>
+                        <p>
+                          <b>Count:</b>
+                          {book.count}
+                        </p>
+                      </CardText>
+                      {/* <Link to={`/bookinfo/${book.bookID}`} > Click to View More</Link> */}
+                      <Button
+                        variant="outlined"
+                        style={{ color: "primary" }}
+                        href={`/bookinfo/${book.bookID}`}
+                      >
+                        {" "}
+                        Click to View More
+                      </Button>
+                    </CardBody>
+                  </Card>
+                </CardDeck>
+              </Col>
+            ))}
+          </Row>
         </Container>
-       </div>
-     )
-   }
-  };
-   export default withRouter(Books)
-
-
-
-
-   
-
+      </div>
+    );
+  }
+}
+export default withRouter(Books);
