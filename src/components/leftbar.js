49a75162cfa98fc8a9fa54ff92cabdcb059f842d
chettykulkarni <chettykulkarni@gmail.com>
2019-12-04 23:04:57 -0800

    quick deploy test

--- src/components/leftbar.js
+++ src/components/leftbar.js
@@ -3,23 +3,41 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {Component} from 'react';
 import '../css/bookinfodesc.css';
+import firebase from 'firebase';
 
 
 
 class LeftBar  extends Component {
-  render(){
+
+  state={request:'',ret:'',mybook:'',isLoggedIn:false}
+
+  componentWillMount = () =>{
+
+    //   firebase.auth().onAuthStateChanged(user=>{
+    //       this.setState({isLoggedIn : !!user });
+    //   });
+    //   alert(this.state.isLoggedIn)
+    // if(this.state.isLoggedIn){
+    //   var mybook=<Nav.Link href="/MyBooks">My Books</Nav.Link>
+    //   this.setState({mybook : mybook})
+    // }
+
     var isAdmin=localStorage.getItem("isAdmin")
     var adminlog=parseInt(isAdmin)
     var request=''
     var ret=''
-    if (adminlog==1){
+    if (adminlog===1){
        request= <Nav.Link href="/Requests">Requests</Nav.Link>
        ret=<Nav.Link href="/Returns">Returns</Nav.Link>
+       this.setState({request:request,ret:ret})
     }
     else{
 
 
     }
+  }
+  render(){
+    
     
 
   return (
@@ -32,8 +50,9 @@ class LeftBar  extends Component {
       >
         <Nav style={{ minHeight: 100 + "vh" }}>
           <li>
-            {request}
-            {ret}
+            {this.state.request}
+            {this.state.ret}
+            {/* {this.state.mybook} */}
             <Nav.Link href="/MyBooks">My Books</Nav.Link>
           </li>
         </Nav>
