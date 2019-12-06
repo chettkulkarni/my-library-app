4eca5f57ca12873ca4d1014a804144966b6f7243
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2019-12-06 01:02:08 -0800

    added unit test and snap shot test.

--- src/unit_tests/Requests.test.js
+++ src/unit_tests/Requests.test.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Requests from "../components/Requests";
+import { BrowserRouter } from "react-router-dom";
 
+jest.mock("firebase", () => ({
+  auth: () => {
+    return { onAuthStateChanged: val => true };
+  }
+}));
 test("Snapshot for Requests", () => {
-  // const { container } = render(<Requests />);
-  // expect(container).toMatchSnapshot();
+  const { container } = render(
+    <BrowserRouter>
+      <Requests />
+    </BrowserRouter>
+  );
+  expect(container).toMatchSnapshot();
   console.log(6);
 });
